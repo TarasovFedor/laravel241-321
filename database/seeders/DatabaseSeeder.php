282224507dcf0bc7f9f85786923c2cd5b2b4c8ff
TarasovFedor <fedor.tarasov.2007@mail.ru>
2025-09-16 15:07:18 +0300

    la4 4 finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,12 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        \App\Models\Article::factory(10) -> create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
     }
 }
+
+// php artisan db:seed
